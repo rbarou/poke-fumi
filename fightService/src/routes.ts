d0408409d756067cadd64e07a3c48466df7a262c
Cynthia A <cynparivony@yahoo.fr>
2022-02-23 00:08:11 +0100

    init Fight Service

---
+++ fightService/src/routes.ts
@@ -0,0 +1,53 @@
+import * as express from "express";
+//import * as AdminController from "./adminController";
+//import {User} from './model';
+//import * as UserController from './userController';
+
+
+export const register = (app: express.Application) => {
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (_,res) => res.send('Hello world from player service'));
+
+    app.get('/user/getAllUsers',  (_,res) => {
+        res.status(200).json(UserController.listUsers());
+    });
+
+    app.get('/user/getUserByName', (req,res) => {
+        const name = req.query.name as string;
+        if(name){
+            res.status(200).json(UserController.getUserByName(name));
+        }else{
+            res.status(400).json("Please specify a username");
+        }
+    });
+
+    app.get('/user/getUserById', (req,res) => {
+        const id = req.query.id as string;
+        if(id){
+            res.status(200).json(UserController.getUserById(id));
+        }else{
+            res.status(400).json("Please specify an id");
+        }
+    });
+
+    app.post('/user/register', (req,res) => {
+        const newUser: User = req.body;
+        const userName = UserController.getUserByName(newUser.name);
+        if(userName){
+            res.status(400).json("This username is already taken");
+        }else{
+            res.status(200).json(UserController.addUser(newUser.name,newUser.password));
+        }
+    });
+
+    app.post('/user/connect', (req, res) => {
+        const {name,password} = req.body;
+        const user : User = UserController.login(name,password);
+        if(user){
+          res.status(200).json(user);
+        }else{
+          res.status(400).send("Invalid username or password, please try again...")
+        }
+    });
